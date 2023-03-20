6ea957ddf79d1728662669a7d22f62c57da25111
prongbang <prongbang@gmail.com>
2023-03-20 13:03:37 +0700

    Support null safety

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
-## [1.0.0] - 2020/01/29
+## [1.0.2] - 2023/03/20
+
+* Support null safety.
+
+## [1.0.1] - 2020/01/29
 
 * FittedText widget auto size.
 
-## [1.0.1] - 2020/01/29
+## [1.0.0] - 2020/01/29
 
 * FittedText widget auto size.
--- lib/fitted_text.dart
+++ lib/fitted_text.dart
@@ -4,26 +4,27 @@ import 'package:flutter/material.dart';
 
 class FittedText extends StatelessWidget {
   final String message;
-  final TextStyle style;
-  final double width;
+  final TextStyle? style;
+  final double? width;
   final double height;
   final double widthPercent;
-  final Color backgroundColor;
-  final Decoration decoration;
+  final Color? backgroundColor;
+  final Decoration? decoration;
   final Alignment alignment;
   final EdgeInsetsGeometry padding;
 
-  const FittedText(this.message,
-      {Key key,
-      this.style,
-      this.height = 20,
-      this.width,
-      this.widthPercent = 100,
-      this.backgroundColor,
-      this.decoration,
-      this.alignment = Alignment.centerLeft,
-      this.padding = const EdgeInsets.all(0)})
-      : super(key: key);
+  const FittedText(
+    this.message, {
+    Key? key,
+    this.style,
+    this.height = 20,
+    this.width,
+    this.widthPercent = 100,
+    this.backgroundColor,
+    this.decoration,
+    this.alignment = Alignment.centerLeft,
+    this.padding = const EdgeInsets.all(0),
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
